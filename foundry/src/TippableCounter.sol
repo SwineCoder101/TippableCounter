1db67db107bf34db41063a7f0d8f01af96b9bd9a
Liam Faruq <liam@Liams-MacBook-Pro.local>
2024-02-27 19:25:32 +0000

    add-contract

---
+++ foundry/src/TippableCounter.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+import "solmate/auth/Owned.sol";
+
+contract TippableCounter is Owned {
+    uint256 counter;
+
+    event Incremented(address indexed sender, uint8 incrementedBy, uint256 ethSent);
+
+    constructor() Owned(msg.sender) {}
+
+    function increment(uint8 incrementBy) external payable {
+        require(msg.value >= 0.001 ether, "Fee required.");
+        require(incrementBy >= 1 && incrementBy <= 10, "incrementBy not in range.");
+        counter += incrementBy;
+
+        emit Incremented(msg.sender, incrementBy, msg.value);
+    }
+
+    function withdraw() external onlyOwner {
+        counter = 0;
+        (bool sent,) = address(owner).call{value: address(this).balance}("");
+    }
+}
